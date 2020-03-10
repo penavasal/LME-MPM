c68e252c2b23b28c81fbc4a7db9b97ad226449d0
Miguel Molinos Pérez <m.molinos@alumnos.upm.es>
2020-03-10 17:28:36 +0100

    Cambios 10-3-2020

--- auto/main.el
+++ auto/main.el
@@ -5,12 +5,12 @@
                      '(("svjour3" "twocolumn")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("subfig" "lofdepth" "lotdepth") ("units" "ugly")))
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
@@ -73,6 +73,7 @@
     "eq:particle_body_forces"
     "eq:particle_load_forces"
     "eq:Approx-velocity-mesh"
+    "eq:Approx-gradvelocity-mesh"
     "eq:particle_balance_forces3"
     "eq:particle_nod_mass"
     "eq:nodal_internal_forces"
