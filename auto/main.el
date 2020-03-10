4856d4173c7f70bdeaf49241edb32dd8aa8b4927
Miguel <m.molinos@alumnos.upm.es>
2020-03-10 20:04:58 +0100

    Cambios II 10-3-2020

--- auto/main.el
+++ auto/main.el
@@ -5,16 +5,17 @@
                      '(("svjour3" "twocolumn")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("subfig" "lofdepth" "lotdepth") ("units" "ugly")))
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
     "Figures/Dyka_bar"
+    "Figures/Block"
     "fix-cm"
     "svjour3"
     "svjour310"
@@ -113,6 +114,7 @@
     "fig:LME_7.0_Shape_Fun"
     "fig:LME_5.0_Shape_Fun"
     "fig:LME_MPM"
+    "fig:block"
     "sec:study-error"
     "eq:RMS"
     "fig:Dyka-MPM-uGIMP-LME"
